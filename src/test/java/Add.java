9018f4a43d8a566b3d399b446848e7fd20d1bfb1
shirleyxyli <xiaoyan.li@talebase.com>
2016-08-24 10:00:44 +0800

    fcommit

---
+++ src/test/java/Add.java
@@ -0,0 +1,9 @@
+/**
+ * Created by xiaoyan.li on 2016-8-23.
+ */
+public class Add {
+
+    public int addtest(int num1, int num2) {
+        return num1+num2;
+    }
+}
---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,22 @@
+import cucumber.annotation.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ * Created by shirley.li on 2016-8-23.
+ */
+public class FirstTest {
+    public Add a;
+    @Before
+    public void create(){
+        a = new Add();
+    }
+
+    @Test
+    public void test(){
+        a = new Add();
+        assertEquals(0,a.addtest(0,0));
+        System.out.println("test is successful");
+        assertEquals(10,a.addtest(1,9));
+    }
+}
